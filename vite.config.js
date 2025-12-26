b05d7dd47277a4a7bae6bb7ac8c936a6697fe412
Novamxter <mehramkmohit968@gmail.com>
2025-12-26 11:18:01 +0530

    Fixing deploy bug

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/speaking-assessment-report-ui"
+  base: "/speaking-assessment-report-ui/"
 });
